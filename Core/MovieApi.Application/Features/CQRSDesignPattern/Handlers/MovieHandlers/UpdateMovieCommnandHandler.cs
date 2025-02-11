5720ab461c6bf6108d5648e2592da17b09d99f97
bhmberkan <berkanburakturgut@gmail.com>
2025-02-11 20:08:48 +0300

    Command Sınıflarını oluşturduk.(Category ve Movie için) Handlers sınıflarını oluşutrup içeriklerini kodladık.(CRUD) Queries yapısını oluşturduk. Result Yapısını oluşutrduk
    Application katmanına Domain ve Persistence katmanını ekledik.(Depenject)
    persistence bağlantı yapacağımız tüm katmanlarda olmalıdır.
    Domain ise tüm katmanların erişebileceği tek katmandır.

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/UpdateMovieCommnandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+using MovieApi.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class UpdateMovieCommnandHandler
+    {
+        private readonly MovieContext _context;
+
+        public UpdateMovieCommnandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async void Handler(UpdateMovieCommand command)
+        {
+            var value = await _context.Movies.FindAsync(command.MovieId);
+            value.Title = command.Title;
+            value.CoverImageUrl = command.CoverImageUrl;
+            value.Rating = command.Rating;
+            value.Description = command.Description;
+            value.Duration = command.Duration;
+            value.RelaseDate = command.RelaseDate;
+            value.CreatedYear = command.CreatedYear;
+            value.Status = command.Status;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
